68ede032c8d8d98187c0f44346861a9efeebf008
Matheiu Perdido <matheiuamiel@gmail.com>
2024-02-04 21:51:15 +0800

    added login+register

---
+++ website/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=24, widget=forms.TextInput(attrs={'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=24, widget=forms.TextInput(attrs={'placeholder':'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+
+        self.fields['last_name'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+
+        self.fields['email'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+
+        self.fields['username'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class=""><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="ml-6 my-2 text-black/40 list-disc text-sm"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+
+        self.fields['password2'].widget.attrs['class'] = 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 my-4'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class=""><small>Enter the same password as before, for verification.</small></span>'	
--- website/views.py
+++ website/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+def home(req):
+    # Check if user is logging in
+    if req.method == 'POST':
+        username = req.POST['username']
+        password = req.POST['password']
+        # authenticate user
+        user = authenticate(req, username=username, password=password)
+        if user is not None:
+            login(req, user)
+            messages.success(req, "You have been logged in.")
+            return redirect('home')
+        else:
+            messages.success(req, "There was an error logging in.")
+            return redirect('home')
+
+    else:
+        return render(req, 'home.html', {})
+
+def login_user(req):
+    pass
+
+def logout_user(req):
+    logout(req)
+    messages.success(req, "Logout Successful!")
+    return redirect('home')
+
+def register_user(req):
+    if req.method == 'POST':
+        form = SignUpForm(req.POST)
+        if form.is_valid():
+            form.save()
+            # Log user in after registration
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(req, user)
+            messages.success(req, "Registration Successful! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(req, 'register.html', {'form':form})
+    
+    return render(req, 'register.html', {'form':form})
